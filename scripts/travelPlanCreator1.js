c8450c95bf4704ebb9840fe92064322edc984f19
Justin Ng <jng193@my.bcit.ca>
2022-03-09 01:05:38 -0800

    populated travelPlanCreator1 with fillers

--- scripts/travelPlanCreator1.js
+++ scripts/travelPlanCreator1.js
@@ -1,13 +1,26 @@
-function readFiller() {
-    db.collection("fillers").doc("tjvmfG2ZE0Mi5coBeVX8").onSnapshot(fillerData => {
-        console.log("Coffee Detail: " + fillerData.data());
-        document.getElementById("coffee_details").innerHTML = fillerData.data().Details;
-        document.getElementById("coffee_start").innerHTML = fillerData.data().Start_time;
-        document.getElementById("coffee_end").innerHTML = fillerData.data().End_time;
-        document.getElementById("coffee_location").innerHTML = fillerData.data().Location;
-        document.getElementById("coffee_address").innerHTML = fillerData.data().Address;
-        document.getElementById("coffee_img").src = fillerData.data().Image;
+function populateCardsDynamically() {
+    let fillerTemplate = document.getElementById("fillerTemplate");
+    let fillerGroup = document.getElementById("fillerGroup");
+    
+    db.collection("fillers").get()
+        .then(allFillers => {
+            allFillers.forEach(doc => {
+                var fillerDetail = doc.data().Details;
+                var fillerStart = doc.data().Start;
+                var fillerEnd = doc.data().End;
+                var fillerLocation = doc.data().Location;
+                var fillerAddress = doc.data().Address;
+                var fillerImage = doc.data().Image;
+                let fillerCard = fillerTemplate.content.cloneNode(true);
+                fillerCard.querySelector('#details').innerHTML = fillerDetail;
+                fillerCard.querySelector('#start').innerHTML = fillerStart;
+                fillerCard.querySelector('#end').innerHTML = fillerEnd;
+                fillerCard.querySelector('#location').innerHTML = fillerLocation;
+                fillerCard.querySelector('#address').innerHTML = fillerAddress;
+                fillerCard.querySelector('#img').src = `./images/${fillerImage}.jpg`;
+                fillerGroup.appendChild(fillerCard);
+            })
 
-    });
+        })
 }
-readFiller();
+populateCardsDynamically();
