c8b74db5602c9cbef4828e51483cfd5d38b7658a
Amadeus Min <ama.m.cantabile@gmail.com>
2022-03-15 16:56:27 -0700

    Added update features for  profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -16,17 +16,19 @@ function insertName() {
             currentUser.get()
                 .then(userDoc => {
                     var user_Name = userDoc.data().name;
-                    var user_Email = userDoc.data().email;
-                    console.log(user_Name);
-                    console.log(user_Email);
+                    var user_address = userDoc.data().address;
+                    var user_city = userDoc.data().city;
+                    var user_province = userDoc.data().province;
+                    var user_postalCode = userDoc.data().province;
+                    //var user_Email = userDoc.data().email;
+                    
                     //method #1:  insert with html only
                     //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
                     //method #2:  insert using jquery
                     // $("#name-goes-here").value(user_Name);                         //using jquery
                     // $("#email-goes-here").text(user_Email);
 
-                    document.getElementById("name-goes-here").value = user_Name;
-                    document.getElementById("email-goes-here").value = user_Email;
+                    document.getElementById("inputName").value = user_Name;
 
                 })
         } else {
@@ -35,3 +37,32 @@ function insertName() {
     });
 }
 insertName();
+
+function saveUserInfo() {
+    userName = document.getElementById('inputName').value;       //get the value of the field with id="nameInput"
+    userAddress = document.getElementById('inputAddress').value;     //get the value of the field with id="schoolInput"
+    userCity = document.getElementById('inputCity').value;       //get the value of the field with id="cityInput"
+    userProvince = document.getElementById('inputProvince').value;
+    userPostalCode = document.getElementById('inputPostalCode').value;
+
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            // go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+
+            // write/update the databse
+            currentUser.update({
+                name: userName,
+                address: userAddress,
+                city: userCity,
+                province: userProvince,
+                postalCode: userPostalCode
+            })
+                .then(() => {
+                    console.log("Document successfully updated!");
+                    //document.getElementById('personalInfoFields').disabled = true;
+                })
+        }
+    })
+}
