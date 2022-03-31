c98e2788aab0c63fe11ad41b1678cf9b348a946e
Jin Rong Jian <jrong8018@gmail.com>
2022-03-31 10:23:44 -0700

    updated travePlan.js

--- scripts/travelPlan.js
+++ scripts/travelPlan.js
@@ -10,58 +10,16 @@ async function populateSchedule() {
         if (user) {
 
             //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").orderBy("name", "desc");//.doc(user.uid);
+            currentUser = db.collection("users").doc(user.uid)
             //get the document for current user.
-            //currentUser
-            db.collection("users").doc(user.uid)
-                //.orderBy("schedule").collection("womensFreeskiBigAir").orderBy("startTime")
-                .get()
+            currentUser.get()
                 .then(userDoc => {
-                    if (userDoc.exists) {
-                        console.log("exists");
-                        console.log(userDoc);
-                    }
-                    //userDoc.orderBy("schedule");
-                    // var i = 0;
-                    // userDoc.forEach(doc => {
-                    //     console.log(doc.data());
-                    // })
-
-
-                    //get the data fields of the user-
+                    //get the data fields of the user
                     var userName = userDoc.data().name;
                     var userSchedule = userDoc.data().schedule;
                     var scheduleList = "";
                     console.log(user.uid);
 
-                    arrayData = [];
-
-                    userSchedule[0].get().then(test => {
-                        console.log(test.data());
-                    });
-
-                    // userSchedule.get().then(testData=>{
-                    //     console.log(testData.data());
-                    // })
-
-                    console.log(arrayData);
-
-                    // userSchedule.sort(function (a, b){
-                    //     //console.log(a.startTime);
-                    //     return parseInt(a.startTime) - parseInt(b.startTime);
-                    // })
-
-
-                    for (var i = 0; i < userSchedule.length; i++) {
-                        userSchedule[i].get()
-                            .then(scheduleDoc => {
-                                // let testData = scheduleDoc.data().startTime.split(":")
-                                // console.log(testData[0]);
-                                scheduleList += '<ul class="list-group-item"><div class="d-flex w-100 justify-content-between" ><div class="d-flex flex-column bd-highlight mb-3"><h5 class="mb-1">'
-                                    + scheduleDoc.data().event + '</h5><br /><h6 class="mb-4">Time: ' + scheduleDoc.data().startTime + "-" + scheduleDoc.data().endTime + '</h6><p class="mb-1">Location: ' + scheduleDoc.data().location
-                                    + '</p></div><small>3 days ago</small><img src="./images/Vancouver.jpg" style="width: 20%; height:150px;"></div></ul >';
-
-                                document.getElementById("schedule-list-container").innerHTML = scheduleList;
                     let planTemplate = document.getElementById("planTemplate");
                     let planGroup = document.getElementById("planGroup");
 
@@ -175,7 +133,7 @@ function addFiller() {
 //-----------------------------------------------------------------------------
 // This function is called when the user clicks on the "Finish" button after 
 // seleting the sport event they are going for the date.  It then filters the
-// filler collection to determine all the filler events that fit before the first
+// filler collection to determine all the filler events that fits before the first
 // sport event, in between the gap of sport events, and after the last sport event
 // of the day. Then it invoke the addFillerToSavedPlan() function.
 //-----------------------------------------------------------------------------
@@ -236,16 +194,11 @@ function filterFillerForSportEvent() {
     })
 }
 
-
 //-----------------------------------------------------------------------------
 // This function adds the filtered filler events of the day to the savedPlan
 // subcollection of the logged-in user as new generated documents.
 //-----------------------------------------------------------------------------
 function addFillerToSavedPlan(fillerList) {
-
-    // Get slected filter data from the local storage and use as the filler date
-    let fillerDate = localStorage.getItem("filterDate");
-
     console.log(fillerList);
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -254,7 +207,6 @@ function addFillerToSavedPlan(fillerList) {
             for (var i = 0; i < fillerList.length; i++) {
                 currentUser.collection("savedPlan").add({
                     event: fillerList[i].Details,
-                    date: fillerDate,
                     location: fillerList[i].Location,
                     address: fillerList[i].Address,
                     start: fillerList[i].Start,
@@ -284,33 +236,33 @@ callFunction();
 // addFiller2();
 filterFillerForSportEvent();
 
-// function deletePlan() {
-//     if (confirm("Are you sure you want to delete the plan? Click 'Ok' to delete.")) {
-//         firebase.auth().onAuthStateChanged(user => {
-//             if (user) {
-//                 var currentUser = db.collection("users").doc(user.uid);
-//                 currentUser.collection("savedPlan").get()
-//                     .then(plan => {
-//                         plan.forEach(doc => {
-//                             var docToDelete = doc.id;
-//                             currentUser.collection("savedPlan").doc(docToDelete).delete();
-//                         })
-//                         document.getElementById("planGroup").remove();
-//                         document.getElementById("planMessage").innerHTML = "The Plan Has Been Deleted!";
-//                         document.getElementById("planButton").innerHTML = "Create Another Plan";
-//                         document.getElementById("planButton").setAttribute("href", "travelPlanCreator1.html");
-//                         document.getElementById("planButton").setAttribute("onclick", "");
-//                         console.log("all are deleted");
-//                     })
+function deletePlan() {
+    if (confirm("Are you sure you want to delete the plan? Click 'Ok' to delete.")) {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                var currentUser = db.collection("users").doc(user.uid);
+                currentUser.collection("savedPlan").get()
+                    .then(plan => {
+                        plan.forEach(doc => {
+                            var docToDelete = doc.id;
+                            currentUser.collection("savedPlan").doc(docToDelete).delete();
+                        })
+                        document.getElementById("planGroup").remove();
+                        document.getElementById("planMessage").innerHTML = "The Plan Has Been Deleted!";
+                        document.getElementById("planButton").innerHTML = "Create Another Plan";
+                        document.getElementById("planButton").setAttribute("href", "travelPlanCreator1.html");
+                        document.getElementById("planButton").setAttribute("onclick", "");
+                        console.log("all are deleted");
+                    })
 
 
-//             } else {
-//                 windows.location.assign("login.html");
-//             }
-//         }
+            } else {
+                windows.location.assign("login.html");
+            }
+        }
 
-//         )
-//     } else {
-//         window.location.href = "#";
-//     }
-// }
+        )
+    } else {
+        window.location.href = "#";
+    }
+}
