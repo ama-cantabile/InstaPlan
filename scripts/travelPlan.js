0b62f66761f3a9b81452c40ad50511732db1c78f
Jin Rong Jian <jrong8018@gmail.com>
2022-03-31 10:48:24 -0700

    linked sports event selection date to filler date

--- scripts/travelPlan.js
+++ scripts/travelPlan.js
@@ -25,42 +25,6 @@ async function populateSchedule() {
 
                     currentUser.collection("savedPlan").orderBy('start').get()
                         .then(plan => {
-                            // for (var i = 0; i < plan.size; i++) {
-                            //     var gap = plan.docs[i + 1].data().Start - plan.docs[i].data().End;
-
-                            //     db.collection("fillers").where("Start", "==", "10:00").where("End", "==", "11:00")
-                            //         .get()
-                            //         .then(test => {
-                            //             console.log(test.docs[0].data());
-                            //             currentUser.collection("savedPlan").add({
-                            //                 Event: test.docs[0].data().Details,
-                            //                 Date: "Mar 27",
-                            //                 Location: test.docs[0].data().Location,
-                            //                 Start: test.docs[0].data().Start,
-                            //                 End: test.docs[0].data().End,
-                            //                 Img: test.docs[0].data().Image,
-                            //             })
-                            //         });
-                            // }
-                            // console.log(plan.docs[0].data());
-                            // console.log(plan.size);
-                            // var gap = plan.docs[1].data().Start - plan.docs[0].data().End;
-                            // console.log(gap);
-
-                            // db.collection("fillers").where("Start", "==", "10:00").where("End", "==", "11:00")
-                            //     .get()
-                            //     .then(test => {
-                            //         console.log(test.docs[0].data());
-                            //         currentUser.collection("savedPlan").add({
-                            //             Event: test.docs[0].data().Details,
-                            //             Date: "Mar 27",
-                            //             Location: test.docs[0].data().Location,
-                            //             Start: test.docs[0].data().Start,
-                            //             End: test.docs[0].data().End,
-                            //             Img: test.docs[0].data().Image,
-                            //         })
-                            //     });
-
                             plan.forEach(doc => {
                                 var event = doc.data().event;
                                 var date = doc.data().date;
@@ -120,15 +84,6 @@ function addFiller() {
     })
 
 }
-
-// async function addFiller2() {
-
-//     var fillerSortedByStart = await sortFillerByStart();
-//     console.log(fillerSortedByStart);
-//     // await sortFillerByEnd(fillerSortedByStart);
-
-// }
-
 
 //-----------------------------------------------------------------------------
 // This function is called when the user clicks on the "Finish" button after 
@@ -199,6 +154,10 @@ function filterFillerForSportEvent() {
 // subcollection of the logged-in user as new generated documents.
 //-----------------------------------------------------------------------------
 function addFillerToSavedPlan(fillerList) {
+
+    // Get slected filter data from the local storage and use as the filler date
+    let fillerDate = localStorage.getItem("filterDate");
+
     console.log(fillerList);
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -207,6 +166,7 @@ function addFillerToSavedPlan(fillerList) {
             for (var i = 0; i < fillerList.length; i++) {
                 currentUser.collection("savedPlan").add({
                     event: fillerList[i].Details,
+                    date: fillerDate,
                     location: fillerList[i].Location,
                     address: fillerList[i].Address,
                     start: fillerList[i].Start,
@@ -220,8 +180,6 @@ function addFillerToSavedPlan(fillerList) {
 
 }
 
-
-
 //call the function to run it 
 function callFunction() {
     setTimeout(function () {
@@ -229,11 +187,7 @@ function callFunction() {
     }, 3000);
 }
 
-
 callFunction();
-// addFiller();
-// populateSchedule();
-// addFiller2();
 filterFillerForSportEvent();
 
 function deletePlan() {
