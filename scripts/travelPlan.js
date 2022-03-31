68a74b315195e4c86471d6a56247e9e047db2dcd
Justin Ng <jng193@my.bcit.ca>
2022-03-31 10:08:42 -0700

    update

--- scripts/travelPlan.js
+++ scripts/travelPlan.js
@@ -10,60 +10,16 @@ async function populateSchedule() {
         if (user) {
 
             //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").orderBy("name", "desc");//.doc(user.uid);
+            currentUser = db.collection("users").doc(user.uid)
             //get the document for current user.
-            //currentUser
-            db.collection("users").doc(user.uid)
-                //.orderBy("schedule").collection("womensFreeskiBigAir").orderBy("startTime")
-                .get()
+            currentUser.get()
                 .then(userDoc => {
-                    if (userDoc.exists) {
-                        console.log("exists");
-                        console.log(userDoc);
-                    }
-                    //userDoc.orderBy("schedule");
-                    // var i = 0;
-                    // userDoc.forEach(doc => {
-                    //     console.log(doc.data());
-                    // })
-
-
-                    //get the data fields of the user-
+                    //get the data fields of the user
                     var userName = userDoc.data().name;
                     var userSchedule = userDoc.data().schedule;
                     var scheduleList = "";
                     console.log(user.uid);
 
-<<<<<<< HEAD
-                    arrayData = [];
-
-                    userSchedule[0].get().then(test => {
-                        console.log(test.data());
-                    });
-
-                    // userSchedule.get().then(testData=>{
-                    //     console.log(testData.data());
-                    // })
-
-                    console.log(arrayData);
-
-                    // userSchedule.sort(function (a, b){
-                    //     //console.log(a.startTime);
-                    //     return parseInt(a.startTime) - parseInt(b.startTime);
-                    // })
-
-
-                    for (var i = 0; i < userSchedule.length; i++) {
-                        userSchedule[i].get()
-                            .then(scheduleDoc => {
-                                // let testData = scheduleDoc.data().startTime.split(":")
-                                // console.log(testData[0]);
-                                scheduleList += '<ul class="list-group-item"><div class="d-flex w-100 justify-content-between" ><div class="d-flex flex-column bd-highlight mb-3"><h5 class="mb-1">'
-                                    + scheduleDoc.data().event + '</h5><br /><h6 class="mb-4">Time: ' + scheduleDoc.data().startTime + "-" + scheduleDoc.data().endTime + '</h6><p class="mb-1">Location: ' + scheduleDoc.data().location
-                                    + '</p></div><small>3 days ago</small><img src="./images/Vancouver.jpg" style="width: 20%; height:150px;"></div></ul >';
-
-                                document.getElementById("schedule-list-container").innerHTML = scheduleList;
-=======
                     let planTemplate = document.getElementById("planTemplate");
                     let planGroup = document.getElementById("planGroup");
 
@@ -121,7 +77,6 @@ async function populateSchedule() {
                                 planCard.querySelector('#location').innerHTML = location;
                                 planCard.querySelector('#img').src = `./images/${img}.jpg`;
                                 planGroup.appendChild(planCard);
->>>>>>> 708f49e5f7cc90571619934b106cdc2539925fa0
                             })
 
                         })
